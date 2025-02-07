14d34e4332fc50c8bebcd44788f89a94d99858ab
Jinkkuu <reddabomb82@gmail.com>
2025-02-07 16:37:15 -0500

    Added Now playing labels in the home screen as shown in the concept draft

--- Script/home.cs
+++ Script/home.cs
@@ -9,12 +9,20 @@ using System.Collections.Generic;
 
 public partial class home : Node
 {
+	public Label SongTitle { get; set; }
+	public Label SongArtist { get; set; }
+	public static SettingsOperator SettingsOperator { get; set; }
 	public override void _Ready()
 	{	
+		SettingsOperator = GetNode<SettingsOperator>("/root/SettingsOperator");
+		SongTitle = GetNode<Label>("./Titlesong");
+		SongArtist = GetNode<Label>("./Descsong");
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		SongTitle.Text = SettingsOperator.Sessioncfg["beatmaptitle"]?.ToString() ?? "No song selected";
+		SongArtist.Text = SettingsOperator.Sessioncfg["beatmapartist"]?.ToString() ?? "";
 	}
 	private void _play(){
 		GetTree().ChangeSceneToFile("res://Panels/Screens/song_select.tscn");
