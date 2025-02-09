b1652ad8ac6cc317da8ae29842a9ea3c34ad5cca
Jinkkuu <reddabomb82@gmail.com>
2025-02-09 15:00:04 -0500

    Parallax background added

--- Script/BeatmapBackground.cs
+++ Script/BeatmapBackground.cs
@@ -6,11 +6,12 @@ public partial class BeatmapBackground : TextureRect
 	// Called when the node enters the scene tree for the first time.
 	public static SettingsOperator SettingsOperator { get; set; }
 	public TextureRect self { get ;set; }
+	Texture2D bg {get;set;}
 	public override void _Ready()
 	{
 		SettingsOperator = GetNode<SettingsOperator>("/root/SettingsOperator");
 		self = GetNode<TextureRect>(".");
-		Texture2D bg = (Texture2D)SettingsOperator.Sessioncfg["background"];
+		bg = (Texture2D)SettingsOperator.Sessioncfg["background"];
 		if (bg != null){
 		self.Texture = bg;
 		};
@@ -19,5 +20,9 @@ public partial class BeatmapBackground : TextureRect
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double _delta)
 	{
+		Vector2 mousePos = GetViewport().GetMousePosition();
+		Vector2 screensize = GetViewportRect().Size;
+		self.Position = new Vector2((mousePos[0]/screensize.X*10)-10, (mousePos[1]/screensize.Y*10)-10);
+
 	}
 }
