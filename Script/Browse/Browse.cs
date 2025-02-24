ecc98dbc956070139fb94176e5498f96823be060
Jinkkuu <reddabomb82@gmail.com>
2025-02-24 14:04:19 -0500

    Make card bigger

--- Script/Browse/Browse.cs
+++ Script/Browse/Browse.cs
@@ -7,13 +7,9 @@ using System.Collections.Generic;
 
 public class CatalogCardLegend {
 	public string cover { get; set; }
-	public string cover2x { get; set; }
 	public string card { get; set; }
-	public string card2x { get; set; }
 	public string list { get; set; }
-	public string list2x { get; set; }
 	public string slimcover { get; set; }
-	public string slimcover2x { get; set; }
 }
 public class BrowseCatalogLegend {
 	public int id { get; set; }
