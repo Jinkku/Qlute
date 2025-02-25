cc19083a7069776865fab3f13b329d50e635017b
Jinkkuu <reddabomb82@gmail.com>
2025-02-24 20:16:17 -0500

    Make the home screen button bounce!

---
+++ Script/Effects/ButtonBounce.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class ButtonBounce : Button
+{
+    private void _focus(){
+        GD.Print("hi");
+		var _tween = GetTree().CreateTween();
+			_tween.TweenProperty(this, "custom_minimum_size", new Vector2(180,95), 0.2f)
+				.SetTrans(Tween.TransitionType.Bounce)
+				.SetEase(Tween.EaseType.Out);
+			_tween.Play();}
+    private void _unfocus(){
+		var _tween = GetTree().CreateTween();
+			_tween.TweenProperty(this, "custom_minimum_size", new Vector2(154,95), 0.2f)
+				.SetTrans(Tween.TransitionType.Bounce)
+				.SetEase(Tween.EaseType.Out);
+			_tween.Play();}
+}
