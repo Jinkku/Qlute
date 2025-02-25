f53f55e60258829f94637c095b5b24bc02858fe2
Jinkkuu <reddabomb82@gmail.com>
2025-02-24 20:33:00 -0500

    Intro~

---
+++ Script/IntroPending.cs
@@ -0,0 +1,54 @@
+using Godot;
+using System;
+
+public partial class IntroPending : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public PanelContainer HomeButtons {get;set;}
+	public Vector2 HomeButtonsPOS {get;set;}
+	public override void _Ready()
+	{
+		HomeButtons = GetNode<PanelContainer>("../HomeButtonBG");
+		HomeButtonsPOS = HomeButtons.Position;
+	}
+	private void _on_bomb_pressed(){
+		HomeButtons.Position = new Vector2(HomeButtons.Position.X,HomeButtons.Position.Y+150);
+		HomeButtons.Modulate = new Color(0f,0f,0f,0f);
+		var tom = 0.3f;
+		var _tween = GetTree().CreateTween();
+			_tween.TweenProperty(this, "position", new Vector2(Position.X,Position.Y-150), tom)
+				.SetTrans(Tween.TransitionType.Cubic)
+				.SetEase(Tween.EaseType.Out);
+			_tween.Play();
+		var _tween2 = GetTree().CreateTween();
+			_tween2.TweenProperty(this, "modulate", new Color(0f,0f,0f,0f), tom)
+				.SetTrans(Tween.TransitionType.Cubic)
+				.SetEase(Tween.EaseType.Out);
+			_tween2.Play();
+		var _tween3 = GetTree().CreateTween();
+			_tween3.TweenProperty(this, "visible", false, tom)
+				.SetTrans(Tween.TransitionType.Cubic)
+				.SetEase(Tween.EaseType.Out);
+			_tween3.Play();
+		var _tween4 = GetTree().CreateTween();
+			_tween4.TweenProperty(HomeButtons, "visible", true, 0f)
+				.SetTrans(Tween.TransitionType.Cubic)
+				.SetEase(Tween.EaseType.Out);
+			_tween4.Play();
+		var _tween5 = GetTree().CreateTween();
+			_tween5.TweenProperty(HomeButtons, "position", HomeButtonsPOS, tom)
+				.SetTrans(Tween.TransitionType.Cubic)
+				.SetEase(Tween.EaseType.Out);
+			_tween5.Play();
+		var _tween6 = GetTree().CreateTween();
+			_tween6.TweenProperty(HomeButtons, "modulate", new Color(1f,1f,1f,1f), tom)
+				.SetTrans(Tween.TransitionType.Cubic)
+				.SetEase(Tween.EaseType.Out);
+			_tween6.Play();
+
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
