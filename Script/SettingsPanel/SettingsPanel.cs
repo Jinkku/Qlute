5c9d8e6e1190f53cb75931187a9b8299518c07dd
Jinkkuu <reddabomb82@gmail.com>
2025-03-01 14:07:07 -0500

    Add catagories

--- Script/SettingsPanel/SettingsPanel.cs
+++ Script/SettingsPanel/SettingsPanel.cs
@@ -5,25 +5,37 @@ public partial class SettingsPanel : Control
 {
 	// Called when the node enters the scene tree for the first time.
 	public static SettingsOperator SettingsOperator { get; set; }
-	public static OptionButton Windowmode { get; set; }
-	public static HSlider BackgroundDim { get; set; }
-	public static Button OffsetButton { get; set; }
-	public static HSlider OffsetSlider { get; set; }
-	public static Label OffsetTicker { get; set; }
+	public OptionButton Windowmode { get; set; }
+	public HSlider BackgroundDim { get; set; }
+	public Button OffsetButton { get; set; }
+	public HSlider OffsetSlider { get; set; }
+	public Label OffsetTicker { get; set; }
+	public ScrollContainer Scrolls { get; set; }
 	public override void _Ready(){
 		SettingsOperator = GetNode<SettingsOperator>("/root/SettingsOperator");
-		Windowmode = GetNode<OptionButton>("ColorRect/ScrollContainer/VBoxContainer/WindowSelector");
+		Windowmode = GetNode<OptionButton>("ColorRect/Panels/Scroll/Sections/WindowSelector");
 		Windowmode.Selected = int.TryParse(SettingsOperator.GetSetting("windowmode")?.ToString(), out int mode) ? mode : 0;
-		BackgroundDim = GetNode<HSlider>("ColorRect/ScrollContainer/VBoxContainer/BackgroundDim");
-		OffsetButton = GetNode<Button>("ColorRect/ScrollContainer/VBoxContainer/AudioOffsetAuto");
-		OffsetSlider = GetNode<HSlider>("ColorRect/ScrollContainer/VBoxContainer/AudioOffset");
-		OffsetTicker = GetNode<Label>("ColorRect/ScrollContainer/VBoxContainer/AudioNotice2");
+		BackgroundDim = GetNode<HSlider>("ColorRect/Panels/Scroll/Sections/BackgroundDim");
+		OffsetButton = GetNode<Button>("ColorRect/Panels/Scroll/Sections/AudioOffsetAuto");
+		OffsetSlider = GetNode<HSlider>("ColorRect/Panels/Scroll/Sections/AudioOffset");
+		OffsetTicker = GetNode<Label>("ColorRect/Panels/Scroll/Sections/AudioNotice2");
+		Scrolls = GetNode<ScrollContainer>("ColorRect/Panels/Scroll");
+		GetNode<Label>("ColorRect/Panels/Scroll/Sections/GodotEngineVersion").Text = $"Godot Version {Engine.GetVersionInfo()["major"]}.{Engine.GetVersionInfo()["minor"]}";
 		BackgroundDim.Value = SettingsOperator.backgrounddim;
 		OffsetButton.Text = "Set offset by last played song (" + SettingsOperator.Getms() + "ms)";
 		var offset = SettingsOperator.GetSetting("audiooffset") != null ? float.Parse(SettingsOperator.GetSetting("audiooffset").ToString()) : 0;
 		OffsetSlider.Value = 200-offset;
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	private void _display(){
+		Scrolls.GetVScrollBar().Value = GetNode<Label>("ColorRect/Panels/Scroll/Sections/Display").Position.Y;
+	}
+	private void _audio(){
+		Scrolls.GetVScrollBar().Value = GetNode<Label>("ColorRect/Panels/Scroll/Sections/Audio").Position.Y;
+	}
+	private void _debug(){
+		Scrolls.GetVScrollBar().Value = GetNode<Label>("ColorRect/Panels/Scroll/Sections/Debug").Position.Y;
+	}
 	public override void _Process(double delta)
 	{
 
