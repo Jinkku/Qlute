1149929f96f9ba896267a03a3be91e1548df71c8
jinkkuu <reddabomb82@gmail.com>
2025-04-06 13:32:30 -0400

    Cleaning

--- Script/Bootstrap.cs
+++ Script/Bootstrap.cs
@@ -12,19 +12,8 @@ public partial class Bootstrap : Control
 		SettingsOperator = GetNode<SettingsOperator>("/root/SettingsOperator");
 		animationPlayer.Play("Intro");
 		SettingsOperator.Sessioncfg["toppanelhide"] = true;
-//	    Home = GD.Load<PackedScene>("res://Panels/Screens/home_screen.tscn").Instantiate().GetNode<Control>(".");
-//		AddChild(Home);
-//		Home.MouseFilter = MouseFilterEnum.Stop;
-//		Home.SetProcessInput(false);
-//		Home.Modulate = new Color(0f,0f,0f,0f);
 	}
 	public void _intro_finished(string animationame){
-//		var _tween = Home.CreateTween();
-//		_tween.Connect("finished",new Callable(this,nameof(_anifin)));
-//			_tween.TweenProperty(Home, "modulate", new Color(1f,1f,1f,1f), 0.5f)
-//				.SetTrans(Tween.TransitionType.Cubic)
-//				.SetEase(Tween.EaseType.Out);
-//			_tween.Play();
 		if (SettingsOperator.Beatmaps.Count>0) {
 			SettingsOperator.SelectSongID(SettingsOperator.RndSongID());
 		}
