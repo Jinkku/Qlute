698504ee26b2a0a6839c983f4905d1aecb2f152b
jinkkuu <reddabomb82@gmail.com>
2025-05-19 15:25:43 -0400

    Polish the menus

--- Script/TopPanel/Settings.cs
+++ Script/TopPanel/Settings.cs
@@ -50,39 +50,46 @@ public partial class Settings : Button
 		return (bool)SettingsOperator.Sessioncfg["showaccountpro"];
 	}
 	private void togglesettingspanel(){
-		if (!(bool)SettingsOperator.Sessioncfg["settingspanelv"]){
-			if (IsInstanceValid(SettingsPanel)){
+		if (chknotifv()){
+			togglenotificationpanel();
+		}
+		if (!(bool)SettingsOperator.Sessioncfg["settingspanelv"])
+		{
+			if (IsInstanceValid(SettingsPanel))
+			{
 				SettingsPanel.QueueFree();
 			}
 			SettingsPanel = GD.Load<PackedScene>("res://Panels/Overlays/Settings.tscn").Instantiate().GetNode<Control>(".");
 			TopPanel.AddChild(SettingsPanel);
 			GetTree().CurrentScene.GetNode(".").SetProcessInput(false);
 			var _tween = SettingsPanel.CreateTween();
-			SettingsPanel.Position = new Vector2(-SettingsPanel.Size.X,50);
-			SettingsPanel.Size = new Vector2(SettingsPanel.Size.X,GetViewportRect().Size[1]-SettingsPanel.Position.Y);
+			SettingsPanel.Position = new Vector2(-SettingsPanel.Size.X, 50);
+			SettingsPanel.Size = new Vector2(SettingsPanel.Size.X, GetViewportRect().Size[1] - SettingsPanel.Position.Y);
 			SettingsPanel.MouseFilter = Control.MouseFilterEnum.Ignore;
 			_tween.SetParallel(true);
-			_tween.TweenProperty(SettingsPanel, "position", new Vector2(0,50), 0.3f)
+			_tween.TweenProperty(SettingsPanel, "position", new Vector2(0, 50), 0.3f)
 				.SetTrans(Tween.TransitionType.Cubic)
 				.SetEase(Tween.EaseType.Out);
-			_tween.TweenProperty(GetTree().CurrentScene, "position", new Vector2(50,0), 0.3f)
+			_tween.TweenProperty(GetTree().CurrentScene, "position", new Vector2(50, 0), 0.3f)
 				.SetTrans(Tween.TransitionType.Cubic)
 				.SetEase(Tween.EaseType.Out);
-			_tween.TweenProperty(GetTree().CurrentScene, "modulate", new Color(0.5f,0.5f,0.5f,1f), 0.3f)
+			_tween.TweenProperty(GetTree().CurrentScene, "modulate", new Color(0.5f, 0.5f, 0.5f, 1f), 0.3f)
 				.SetTrans(Tween.TransitionType.Cubic)
 				.SetEase(Tween.EaseType.Out);
 			_tween.Play();
-		} else if (IsInstanceValid(SettingsPanel)){
+		}
+		else if (IsInstanceValid(SettingsPanel))
+		{
 			var _tween = SettingsPanel.CreateTween();
 			GetTree().CurrentScene.GetNode(".").SetProcessInput(true);
 			_tween.SetParallel(true);
-			_tween.TweenProperty(SettingsPanel, "position", new Vector2(-SettingsPanel.Size.X,50), 0.3f)
+			_tween.TweenProperty(SettingsPanel, "position", new Vector2(-SettingsPanel.Size.X, 50), 0.3f)
 				.SetTrans(Tween.TransitionType.Cubic)
 				.SetEase(Tween.EaseType.Out);
-			_tween.TweenProperty(GetTree().CurrentScene, "position", new Vector2(0,0), 0.3f)
+			_tween.TweenProperty(GetTree().CurrentScene, "position", new Vector2(0, 0), 0.3f)
 				.SetTrans(Tween.TransitionType.Cubic)
 				.SetEase(Tween.EaseType.Out);
-			_tween.TweenProperty(GetTree().CurrentScene, "modulate", new Color(1f,1f,1f,1f), 0.3f)
+			_tween.TweenProperty(GetTree().CurrentScene, "modulate", new Color(1f, 1f, 1f, 1f), 0.3f)
 				.SetTrans(Tween.TransitionType.Cubic)
 				.SetEase(Tween.EaseType.Out);
 			_tween.Play();
@@ -93,26 +100,50 @@ public partial class Settings : Button
 			Prog.QueueFree();
 			}
 	private void togglenotificationpanel(){
-		if (!(bool)SettingsOperator.Sessioncfg["notificationpanelv"]){
-			if (IsInstanceValid(NotificationPanel)){
-				SettingsPanel.QueueFree();
+		if (chksettingsv()){
+			togglesettingspanel();
+		}
+		if (!(bool)SettingsOperator.Sessioncfg["notificationpanelv"])
+		{
+			if (IsInstanceValid(NotificationPanel))
+			{
+				NotificationPanel.QueueFree();
 			}
 			NotificationPanel = GD.Load<PackedScene>("res://Panels/Overlays/NotificationPanel.tscn").Instantiate().GetNode<ColorRect>(".");
-			NotificationPanel.Position = new Vector2(0,50);
+			NotificationPanel.Position = new Vector2(0, 50);
 			var tmp = NotificationPanel.Size;
 			TopPanel.AddChild(NotificationPanel);
-			NotificationPanel.Size = new Vector2(tmp[0],GetViewportRect().Size.Y-NotificationPanel.Position.Y);
+			NotificationPanel.Size = new Vector2(tmp[0], GetViewportRect().Size.Y - NotificationPanel.Position.Y);
 			var _tween = NotificationPanel.CreateTween();
-			_tween.TweenProperty(NotificationPanel, "position", new Vector2(GetViewportRect().Size.X-NotificationPanel.Size.X,50), 0.3f)
+			NotificationPanel.MouseFilter = Control.MouseFilterEnum.Ignore;
+			GetTree().CurrentScene.GetNode(".").SetProcessInput(false);
+			_tween.SetParallel(true);
+			_tween.TweenProperty(NotificationPanel, "position", new Vector2(GetViewportRect().Size.X - NotificationPanel.Size.X, 50), 0.3f)
 					.SetTrans(Tween.TransitionType.Cubic)
 					.SetEase(Tween.EaseType.Out);
-				_tween.Play();
-		} else if (IsInstanceValid(NotificationPanel)){
-			var _tween = NotificationPanel.CreateTween();
-			_tween.TweenProperty(NotificationPanel, "position", new Vector2(GetViewportRect().Size.X,50), 0.3f)
+			_tween.TweenProperty(GetTree().CurrentScene, "position", new Vector2(-50, 0), 0.3f)
 				.SetTrans(Tween.TransitionType.Cubic)
 				.SetEase(Tween.EaseType.Out);
-			_tween.TweenCallback(Callable.From(() => queue_free(NotificationPanel)));
+			_tween.TweenProperty(GetTree().CurrentScene, "modulate", new Color(0.5f, 0.5f, 0.5f, 1f), 0.3f)
+				.SetTrans(Tween.TransitionType.Cubic)
+				.SetEase(Tween.EaseType.Out);
+			_tween.Play();
+		}
+		else if (IsInstanceValid(NotificationPanel))
+		{
+			var _tween = NotificationPanel.CreateTween();
+			GetTree().CurrentScene.GetNode(".").SetProcessInput(true);
+			_tween.SetParallel(true);
+			_tween.TweenProperty(NotificationPanel, "position", new Vector2(GetViewportRect().Size.X, 50), 0.3f)
+				.SetTrans(Tween.TransitionType.Cubic)
+				.SetEase(Tween.EaseType.Out);
+			_tween.TweenProperty(GetTree().CurrentScene, "position", new Vector2(0, 0), 0.3f)
+				.SetTrans(Tween.TransitionType.Cubic)
+				.SetEase(Tween.EaseType.Out);
+			_tween.TweenProperty(GetTree().CurrentScene, "modulate", new Color(1f, 1f, 1f, 1f), 0.3f)
+				.SetTrans(Tween.TransitionType.Cubic)
+				.SetEase(Tween.EaseType.Out);
+			_tween.Play();
 		}
 		SettingsOperator.Sessioncfg["notificationpanelv"] = !(bool)SettingsOperator.Sessioncfg["notificationpanelv"];
 
