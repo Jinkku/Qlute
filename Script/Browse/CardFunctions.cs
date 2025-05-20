b5ef2465b41803d07c72d47c493db0c4110b9bee
jinkkuu <reddabomb82@gmail.com>
2025-05-19 21:34:42 -0400

    Polishing Browse screen

--- Script/Browse/CardFunctions.cs
+++ Script/Browse/CardFunctions.cs
@@ -5,9 +5,14 @@ public partial class CardFunctions : Button
 {
     public async override void _Ready()
     {
-        await Browse.DownloadImage(GetMeta("pic").ToString(), (ImageTexture texture) => {
-				GetNode<TextureRect>("SongBackgroundPreview/BackgroundPreview").Texture = texture;
-			});
-        
+        await Browse.DownloadImage(GetMeta("pic").ToString(), (ImageTexture texture) =>
+        {
+            GetNode<TextureRect>("SongBackgroundPreview/BackgroundPreview").Texture = texture;
+        });
+
+    }
+    private void _download()
+    {
+        Notify.Post("Downloading "+ GetMeta("title").ToString());
     }
 }
