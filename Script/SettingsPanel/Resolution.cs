9048b45b423c1f410deb28aeaaf31929061b36dd
jinkkuu <reddabomb82@gmail.com>
2025-05-30 17:04:46 -0400

    Make resolution option invisible if on mobile or full screen

--- Script/SettingsPanel/Resolution.cs
+++ Script/SettingsPanel/Resolution.cs
@@ -4,18 +4,26 @@ using System;
 public partial class Resolution : OptionButton
 {
 	// Called when the node enters the scene tree for the first time.
-	public static SettingsOperator SettingsOperator { get; set; }
+	public SettingsOperator SettingsOperator { get; set; }
+	public Label ResolutionLabel { get; set; }
 	public override void _Ready()
 	{
+		ResolutionLabel = GetNode<Label>("../WindowNoticeRes");
 		SettingsOperator = GetNode<SettingsOperator>("/root/SettingsOperator");
-		if ((int.TryParse(SettingsOperator.GetSetting("windowmode")?.ToString(), out int mode) ? mode : 0) == 0){
-			
-		}else{
-		Disabled = true;}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if ((int.TryParse(SettingsOperator.GetSetting("windowmode")?.ToString(), out int mode) ? mode : 0) == 0)
+		{
+			Visible = true;
+			ResolutionLabel.Visible = true;
+		}
+		else
+		{
+			Visible = false;
+			ResolutionLabel.Visible = false;
+		}
 	}
 }
