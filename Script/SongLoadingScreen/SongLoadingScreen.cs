1a6779d6a37c48136c8abe2b4ae95e20b7d00d48
jinkkuu <reddabomb82@gmail.com>
2025-07-07 18:15:27 -0400

    FIx Loading screen go haywire

--- Script/SongLoadingScreen/SongLoadingScreen.cs
+++ Script/SongLoadingScreen/SongLoadingScreen.cs
@@ -13,6 +13,7 @@ public partial class SongLoadingScreen : Control
 	private Label SongMapper { get; set; }
 	private Label SongDiff { get; set; }
 	private Label SongRating { get; set; }
+	private PanelContainer InfoBox { get; set; }
 	private PanelContainer PicturePanel { get; set; }
 	private TextureRect PictureObj { get; set; }
 	
@@ -22,20 +23,26 @@ public partial class SongLoadingScreen : Control
 	{
 		SettingsOperator.Marathon = false;
 		SettingsOperator = GetNode<SettingsOperator>("/root/SettingsOperator");
-		PicturePanel = GetNode<PanelContainer>("InfoBox/H/Pic/Previewimage");
-		PicturePanel.PivotOffset = new Vector2(PicturePanel.Size.X / 2, PicturePanel.Size.Y / 2);
-		PictureObj = GetNode<TextureRect>("InfoBox/H/Pic/Previewimage/a/Objimg");
+		InfoBox = GetNode<PanelContainer>("InfoBox");
+		PicturePanel = GetNode<PanelContainer>("Previewimage");
+		PicturePanel.Position = new Vector2(-100, PicturePanel.Position.Y);
+		PictureObj = GetNode<TextureRect>("Previewimage/a/Objimg");
 		PictureObj.Size = new Vector2(PictureObj.Size.X+80, PictureObj.Size.Y);
 		ArtificialLoad =GetNode<Timer>("./Timer");
+		PicturePanel.Modulate = new Color(1f,1f,1f,0f);
+		InfoBox.Modulate = new Color(1f,1f,1f,0f);
 		Animation = CreateTween();
-		Animation.TweenProperty(PicturePanel, "position", new Vector2(-40,0), 0.5f).SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Cubic);
+		Animation.SetParallel(true);
+		Animation.TweenProperty(PicturePanel, "position", new Vector2(0,PicturePanel.Position.Y), 0.5f).SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Cubic);
+		Animation.TweenProperty(InfoBox, "modulate", new Color(1f,1f,1f,1f), 0.5f).SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Cubic);
+		Animation.TweenProperty(PicturePanel, "modulate", new Color(1f,1f,1f,1f), 0.5f).SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Cubic);
 		Animation.Play();
 		Animation.Connect("finished", new Callable(this, nameof(_Animationf)));
-		SongTitle = GetNode<Label>("InfoBox/H/Box/Title");
-		SongArtist = GetNode<Label>("InfoBox/H/Box/Artist");
-		SongDiff = GetNode<Label>("InfoBox/H/Box/Difficulty");
-		SongMapper = GetNode<Label>("InfoBox/H/Box/Creator");
-		SongRating = GetNode<Label>("InfoBox/H/Box/Infos/Sections/Level");
+		SongTitle = GetNode<Label>("InfoBox/Box/Title");
+		SongArtist = GetNode<Label>("InfoBox/Box/Artist");
+		SongDiff = GetNode<Label>("InfoBox/Box/Difficulty");
+		SongMapper = GetNode<Label>("InfoBox/Box/Creator");
+		SongRating = GetNode<Label>("InfoBox/Box/Infos/Sections/Level");
 		SongTitle.Text = SettingsOperator.Sessioncfg["beatmaptitle"]?.ToString() ?? "No Beatmaps Selected";
 		SongArtist.Text = SettingsOperator.Sessioncfg["beatmapartist"]?.ToString() ?? "Please select a Beatmap!";
 		SongMapper.Text = "Creator: " + SettingsOperator.Sessioncfg["beatmapmapper"]?.ToString();
