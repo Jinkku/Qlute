8d23ef20ddb0a29204e5160dd9c0bbcd3319eb08
jinkkuu <reddabomb82@gmail.com>
2025-07-13 18:32:13 -0400

    add a option to name a branch

--- Script/Home/version.gd
+++ Script/Home/version.gd
@@ -4,7 +4,8 @@ extends Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var version = str(ProjectSettings.get_setting("application/config/version"))
+	var branch = str(ProjectSettings.get_setting("application/config/branch"))
 	if version == '0.0.0':
 		set_text('Development Build (Qlute)')
 	else:
-		set_text('Qlute/stable ('+ version +')')
+		set_text('Qlute/' + branch + ' ('+ version +')')
