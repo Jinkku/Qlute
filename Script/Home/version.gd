469a4ca9b9893002a5dba5fb9dcde536fc01737c
jinkkuu <reddabomb82@gmail.com>
2025-07-19 15:33:09 -0400

    Add branch to version string

--- Script/Home/version.gd
+++ Script/Home/version.gd
@@ -6,6 +6,6 @@ func _ready() -> void:
 	var version = str(ProjectSettings.get_setting("application/config/version"))
 	var branch = str(ProjectSettings.get_setting("application/config/branch"))
 	if version == '0.0.0':
-		set_text('Development Build (Qlute)')
+		text = 'Development Build (Qlute)'
 	else:
-		set_text('Qlute/' + branch + ' ('+ version +')')
+		text = 'Qlute/' + branch + ' ('+ version + '-' + branch + ')'
