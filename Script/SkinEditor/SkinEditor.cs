c908261cfee9a3fb37c40bf7f304d46269af031b
Jinkkuu <reddabomb82@gmail.com>
2025-09-01 22:17:33 -0400

    Add Settings to skin

--- Script/SkinEditor/SkinEditor.cs
+++ Script/SkinEditor/SkinEditor.cs
@@ -6,11 +6,13 @@ public partial class SkinEditor : Control
 	Node currentScene;
 	private SettingsOperator SettingsOperator { get; set; }
 	private VBoxContainer Menu { get; set; }
+	private HBoxContainer ViewPoints { get; set; }
 	public override void _Ready()
 	{
 		currentScene = GetTree().CurrentScene;
 		SettingsOperator = GetNode<SettingsOperator>("/root/SettingsOperator");
 		Menu = GetNode<VBoxContainer>("Creativity/VBoxContainer/ScrollContainer/Elements");
+		ViewPoints = GetNode<HBoxContainer>("ToolBar/VBoxContainer/PartA/ViewPoints");
 		if (SettingsOperator.TopPanelPosition > 49)
 		{
 			SettingsOperator.toppaneltoggle();
@@ -20,7 +22,8 @@ public partial class SkinEditor : Control
 	private bool MissingFunction { get; set; } = false;
 	private Container MissingDisclaimer { get; set; } = null;
 
-	private void Check() {
+	private void Check()
+	{
 		if (GetTree().CurrentScene == null && MissingFunction == false)
 		{
 			MissingFunction = true;
@@ -47,4 +50,25 @@ public partial class SkinEditor : Control
 	{
 		Global._SkinStart = !Global._SkinStart;
 	}
+
+	private PanelContainer Settings { get; set; }
+	private void _Game()
+	{
+		if (Settings != null)
+		{
+			Settings.QueueFree();
+			Settings = null;
+		}
+	}
+	private void _Settings()
+	{
+		if (Settings == null)
+		{
+			Settings = GD.Load<PackedScene>("res://Panels/SkinEditorElements/Settings.tscn").Instantiate().GetNode<PanelContainer>(".");
+			AddChild(Settings);
+			Settings.Size = new Vector2(Settings.Size.X, Settings.Size.Y - 150);
+			Settings.Position = new Vector2(Settings.Position.X, 100);
+		}
+	}
+	
 }
