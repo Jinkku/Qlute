26276468dbf0e4f843bdfd3924bdec9589957819
Jinkku <reddabomb82@gmail.com>
2025-09-23 21:16:49 -0400

    Add Clear All button

--- Script/NotificationPanel/NotificationPanel.cs
+++ Script/NotificationPanel/NotificationPanel.cs
@@ -3,21 +3,41 @@ using System;
 
 public partial class NotificationPanel : ColorRect
 {
+	private Button ClearAll { get; set; }
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		foreach (var NotiInfo in NotificationListener.NotificationList){
-    	    var NotiCard = GD.Load<PackedScene>("res://Panels/Overlays/Notification.tscn").Instantiate().GetNode<Button>(".");
+		for (int i = NotificationListener.NotificationList.Count - 1; i >= 0; i--)
+		{
+			var NotiInfo = NotificationListener.NotificationList[i];
+			var NotiCard = GD.Load<PackedScene>("res://Panels/Overlays/Notification.tscn").Instantiate().GetNode<Button>(".");
 			NotiCard.SetMeta("is_popup", false);
 			GetNode<VBoxContainer>("MarginContainer/ScrollContainer/VBoxContainer").AddChild(NotiCard);
-			NotiCard.SetMeta("id", Math.Max(0,(int)NotiInfo.id));
+			NotiCard.SetMeta("id", Math.Max(0, (int)NotiInfo.id));
 			NotiCard.Text = NotiInfo.Title;
+			ClearAll = GetNode<Button>("MarginContainer/ScrollContainer/VBoxContainer/ClearAll Button");
+			ClearAll.Disabled = true;
 		}
 	}
 
+	private void _ClearAll()
+	{
+		var tick = 0;
+		NotificationListener.NotificationList.Clear();
+		NotificationListener.NotificationCards.Clear();
+		foreach (var Node in GetNode<VBoxContainer>("MarginContainer/ScrollContainer/VBoxContainer").GetChildren())
+		{
+			if (tick > 1)
+			{
+				Node.QueueFree();
+			}
+			tick++;
+		}
+	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Size = new Vector2(Size.X,GetViewportRect().Size.Y-(GetNode<ColorRect>("..").Position.Y+50));
+		ClearAll.Disabled = NotificationListener.NotificationList.Count < 1;
+		Size = new Vector2(Size.X, GetViewportRect().Size.Y - (GetNode<ColorRect>("..").Position.Y + 50));
 	}
 }
