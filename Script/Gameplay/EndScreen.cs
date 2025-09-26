d4a849e8ee0e232e640354eb0c41571649d3374c
Jinkku <reddabomb82@gmail.com>
2025-09-26 17:45:00 -0400

    Add sfx and a new animation to end screen

--- Script/Gameplay/EndScreen.cs
+++ Script/Gameplay/EndScreen.cs
@@ -12,7 +12,6 @@ public partial class EndScreen : ColorRect
 	{
 		SettingsOperator = GetNode<SettingsOperator>("/root/SettingsOperator");
 		Tween = CreateTween();
-		Tween.TweenInterval(3);
 		Tween.Connect("finished", new Callable(this, nameof(done)));
 		ComboScreen = GetNode<TextureRect>("ComboScene");
 		if (SettingsOperator.Gameplaycfg.Bad == 0) FC();
@@ -20,7 +19,10 @@ public partial class EndScreen : ColorRect
 		else if (SettingsOperator.Gameplaycfg.Accuracy > 0.69f) Good();
 		else Bad();
 	}
-
+    public override void _PhysicsProcess(double delta)
+    {
+		ComboScreen.PivotOffset = ComboScreen.Size / 2;
+    }
 	private void done()
 	{
 		SettingsOperator.toppaneltoggle();
@@ -31,30 +33,41 @@ public partial class EndScreen : ColorRect
 	private void FC()
 	{
 		ComboScreen.Texture = GD.Load<Texture2D>("res://SelectableSkins/Slia/EndScreen/FC.png");
+		ComboScreen.Scale = new Vector2(0.8f, 0.8f);
 		Modulate = new Color(0f, 0f, 0f, 0f);
-		Tween.TweenProperty(this, "modulate", new Color(1f, 1f, 1f, 1f), 1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
+		Sample.PlaySample("res://SelectableSkins/Slia/Sounds/comboclear.wav");
+		Tween.Parallel().TweenProperty(this, "modulate", new Color(1f, 1f, 1f, 1f), 1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
+		Tween.Parallel().TweenProperty(ComboScreen, "scale", new Vector2(1f, 1f), 1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
 		Tween.TweenInterval(3);
 	}
 	private void Bad()
 	{
 		Modulate = new Color(0f, 0f, 0f, 0f);
+		ComboScreen.Scale = new Vector2(0.8f, 0.8f);
 		ComboScreen.Texture = GD.Load<Texture2D>("res://SelectableSkins/Slia/EndScreen/Bad.png");
-		Tween.TweenProperty(this, "modulate", new Color(1f, 1f, 1f, 1f),1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
-		Tween.TweenProperty(ComboScreen, "rotation", 0.25f,0.4f).SetTrans(Tween.TransitionType.Bounce).SetEase(Tween.EaseType.Out);
+		Sample.PlaySample("res://SelectableSkins/Slia/Sounds/comboclear.wav");
+		Tween.Parallel().TweenProperty(this, "modulate", new Color(1f, 1f, 1f, 1f), 1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
+		Tween.Parallel().TweenProperty(ComboScreen, "scale", new Vector2(1f, 1f), 1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
 		Tween.TweenInterval(3);
 	}
 	private void Good()
 	{
 		Modulate = new Color(0f, 0f, 0f, 0f);
+		ComboScreen.Scale = new Vector2(0.8f, 0.8f);
 		ComboScreen.Texture = GD.Load<Texture2D>("res://SelectableSkins/Slia/EndScreen/Good.png");
-		Tween.TweenProperty(this, "modulate", new Color(1f, 1f, 1f, 1f), 1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
+		Sample.PlaySample("res://SelectableSkins/Slia/Sounds/comboclear.wav");
+		Tween.Parallel().TweenProperty(this, "modulate", new Color(1f, 1f, 1f, 1f), 1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
+		Tween.Parallel().TweenProperty(ComboScreen, "scale", new Vector2(1f, 1f), 1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
 		Tween.TweenInterval(3);
 	}
 	private void MAX()
 	{
 		Modulate = new Color(0f, 0f, 0f, 0f);
+		ComboScreen.Scale = new Vector2(0.8f, 0.8f);
 		ComboScreen.Texture = GD.Load<Texture2D>("res://SelectableSkins/Slia/EndScreen/MAX.png");
-		Tween.TweenProperty(this, "modulate", new Color(1f, 1f, 1f, 1f), 1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
+		Sample.PlaySample("res://SelectableSkins/Slia/Sounds/comboclear.wav");
+		Tween.Parallel().TweenProperty(this, "modulate", new Color(1f, 1f, 1f, 1f), 1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
+		Tween.Parallel().TweenProperty(ComboScreen, "scale", new Vector2(1f, 1f), 1f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.Out);
 		Tween.TweenInterval(3);
 	}
 }
