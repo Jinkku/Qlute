67d1c760f5f1905d78fd7c303581f6a4749eaca0
Jinkku <reddabomb82@gmail.com>
2025-10-02 23:12:33 -0400

    Change song indicator

--- Script/Home/SongIndication.cs
+++ Script/Home/SongIndication.cs
@@ -3,21 +3,34 @@ using System;
 
 public partial class SongIndication : Label
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		ReloadSongText();
-	}
-
+	private Tween Ani { get; set; }
+	private string oldText { get; set; }
+	private string newText { get; set; }
 	private void ReloadSongText()
 	{
 		var Title = SettingsOperator.Sessioncfg["beatmaptitle"]?.ToString() ?? "No song selected";
 		var Artist = SettingsOperator.Sessioncfg["beatmapartist"]?.ToString() ?? "";
 		if (Artist == "")
 		{
-			Text = Title;
-		} else {
-			Text = $"{Artist} - {Title}";
+			newText = Title;
+		}
+		else
+		{
+			newText = $"{Artist} - {Title}";
+		}
+		if (oldText != newText)
+		{
+			var posx = GetViewportRect().Size.X;
+			oldText = newText;
+			Modulate = new Color(1f, 1f, 1f, 0f);
+			Ani?.Kill();
+			Ani = CreateTween();
+			Ani.SetParallel(true);
+			Ani.TweenProperty(this, "modulate:a", 0f, 0.5).SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Cubic);
+			Ani.TweenCallback(Callable.From(() => Text = newText)).SetDelay(0.5);
+			Ani.TweenProperty(this, "modulate:a", 1f, 0.5).SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Cubic).SetDelay(0.5);
+			Ani.Play();
+			
 		}
 	}
 
